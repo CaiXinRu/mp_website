b99f01ccca31af151e00faf7df3a68aed53dfe31
naomi <naomi@program.com.tw>
2025-03-19 17:05:01 +0800

    feat: create next.auth initially

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  env: {
+    AUTH_GITHUB_ID: 'Ov23liY2hAEKsD3Nkene',
+    AUTH_GITHUB_SECRET: 'be1b8c99f2893fc119a625c5cd2b917834aee002',
+  },
+
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
+}
 
 const withNextIntl = createNextIntlPlugin()
 export default withNextIntl(nextConfig)
