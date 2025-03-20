37d0c95942e19398176ad9cd2a1393bb206e3716
naomi <naomi@program.com.tw>
2025-03-20 15:51:38 +0800

    fix: set auth ID & secretID in production env

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
 const nextConfig: NextConfig = {
-  env: {
-    AUTH_GITHUB_ID: 'Ov23liY2hAEKsD3Nkene',
-    AUTH_GITHUB_SECRET: 'be1b8c99f2893fc119a625c5cd2b917834aee002',
-  },
-
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,17 @@
 import NextAuth from 'next-auth'
-import GitHub from 'next-auth/providers/github'
+import GitHubProvider from 'next-auth/providers/github'
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [GitHub],
+  providers: [
+    GitHubProvider({
+      clientId:
+        process.env.NODE_ENV === 'production'
+          ? process.env.AUTH_GITHUB_ID
+          : process.env.AUTH_GITHUB_LOCAL_ID,
+      clientSecret:
+        process.env.NODE_ENV === 'production'
+          ? process.env.AUTH_GITHUB_SECRET
+          : process.env.AUTH_GITHUB_LOCAL_SECRET,
+    }),
+  ],
 })
